f915ff1d88e46a9e5da8f8d3b526bb530e2bbec0
Simone <lemmasim98@gmail.com>
2018-01-27 00:54:32 +0100

    carico

---
+++ JAM/Assets/CharacterScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(0.1f, 0, 0);
+		if(Input.GetButton("Fire1")){
+			AudioSource audio = GetComponent<AudioSource>();
+			audio.Play();
+		}
+	}
+}
