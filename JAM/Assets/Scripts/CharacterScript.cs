51346784f4f19b5ae28c2461e664b945eb7bf294
Simone <lemmasim98@gmail.com>
2018-01-27 22:50:08 +0100

    ca<<o

--- JAM/Assets/Scripts/CharacterScript.cs
+++ JAM/Assets/Scripts/CharacterScript.cs
@@ -1,18 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CharacterScript : MonoBehaviour {
 	public LayerMask mask_ostacoli;
 	public LayerMask mask_ground;
 	public LayerMask mask_secondo;
+	public LayerMask mask_fine;
 
 	Animator anim;
 	
-	float movimento = 0.15f;
+	float movimento = 0.12f;
 
 	//velocità a cui si muove il personaggio
-	float marcia = 0.15f;
+	float marcia = 0.12f;
+	float decelerazione = 0f;
 
 	//timer da attivare in caso di collisione con ostacoli
 	float t = 0f;
@@ -22,21 +25,27 @@ public class CharacterScript : MonoBehaviour {
 
 	Rigidbody2D rb;
 	bool doubleJump = true;
+	bool pressed = false;
+	bool IsGrounded = false;
+	float tempoPressione = 0f;
 	//public LayerMask mask;
 	//Animator anim;
 	// Use this for initialization
 	void Start () {
+
 		anim = GetComponent<Animator>();
-
-
-
 		rb = GetComponent<Rigidbody2D>();
+	
 	}
 	
-	// Update is called once per frame
 	void Update () {
-		transform.Translate(marcia, 0, 0);
-		RaycastHit2D hituff = Physics2D.Raycast(transform.position, Vector2.right, 0.4f, mask_ostacoli);
+		if(marcia>0)
+			transform.Translate(marcia, 0, 0);
+		else{
+			SceneManager.LoadScene("Scenes/Scena1", LoadSceneMode.Single);
+		}
+		marcia-=decelerazione;
+		RaycastHit2D hituff = Physics2D.Raycast(transform.position-new Vector3(0, 0.1f, 0), Vector2.right, 0.5f, mask_ostacoli);
         if (hituff.collider != null) {
         	Debug.Log("Contatto");
         	anim.SetBool("inciamp", true);
@@ -49,9 +58,20 @@ public class CharacterScript : MonoBehaviour {
         }
         if(t<0){
         	anim.SetBool("inciamp", false);
-        	marcia = movimento;
+
+			if(decelerazione==0)
+        		marcia = movimento;
+        	else
+				marcia-=decelerazione;
         	t=0;
+			Physics2D.IgnoreLayerCollision(8, 10, false);
         }
+
+
+		RaycastHit2D hitfine = Physics2D.Raycast(transform.position, Vector2.right, 0.4f, mask_fine);
+		if(hitfine.collider!=null){
+			decelerazione = 0.01f;
+		}
 		//pressione di un tasto negli assi
 		/*if(Input.GetButton("Fire1")){
 			Physics2D.IgnoreLayerCollision(8, 9, true);
@@ -59,16 +79,18 @@ public class CharacterScript : MonoBehaviour {
 			audio.Play();
 		}*/
 
-			bool IsGrounded = false;
+			
 		RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up, 0.8f, mask_ground);
 		RaycastHit2D hitc = Physics2D.Raycast(transform.position, -Vector2.up, 0.8f, mask_secondo);
         if (hit.collider != null) {
         	IsGrounded = true;
         	doubleJump = true;
-        }
+        } else
+         IsGrounded = false;
         	if (hitc.collider == null) {
 					Physics2D.IgnoreLayerCollision(8, 11, true);
-					marcia = 0.15f;
+					/*if(decelerazione==0)
+						marcia = movimento;*/
 	        }
 	        if (!IsGrounded && hitc.collider != null) {
 
@@ -78,25 +100,48 @@ public class CharacterScript : MonoBehaviour {
 	        }
 
 
-		if(Input.GetButton("Fire1") && (IsGrounded || doubleJump)){
-			anim.SetBool("jump", true);
-			float mod = 1;
-			if(!IsGrounded){
 
-			Debug.Log("Doppio salto");
-				doubleJump = false;
-				mod = 1.8f;
-			}
-
-       		rb.velocity = new Vector2(rb.velocity.x, 0);
-        	//Debug.Log(doubleJump);
-			rb.AddForce(new Vector2(0, 200 * mod));
-		}
 		if(IsGrounded){
 			anim.SetBool("jump", false);
 		}
 
 
+		if(Input.GetButtonDown("Fire2"))
+			pressed = true;
+		if(pressed)
+			tempoPressione += Time.deltaTime;
+		if(tempoPressione>0.3){
+			saltoLungo();
+			pressed = false;
+			tempoPressione = 0f;
+		}
+		if(Input.GetButtonUp("Fire2")){
+			saltoNormale();
+			pressed = false;
+			tempoPressione = 0f;
+		}
+
+
+
+
+	}
+
+	void saltoNormale(){
+		if(IsGrounded || doubleJump){
+			anim.SetBool("jump", true);
+			float mod = 1;
+			if(!IsGrounded){
+				doubleJump = false;
+				mod = 1.4f;
+			}
+
+       		rb.velocity = new Vector2(rb.velocity.x, 0);
+        	//Debug.Log(doubleJump);
+			rb.AddForce(new Vector2(0, 300 * mod));
+		}
+	}
+	void saltoLungo(){
+			rb.AddForce(new Vector2(600, 300));
 
 	}
 }
---
+++ JAM/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float marcia = 0.1f;
+	public float mod = 1;
+	public Rigidbody2D rb;
+	public SpriteRenderer render;
+	public bool doubleJump = true;
+	public bool IsGrounded;
+	public float t = 0f;
+	public LayerMask mask_ground;
+	public LayerMask mask_secondo;
+	float decelerazione = 0f;
+	Animator anim;
+	float movimento = 0.12f;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();			//rigidBody initialization
+		render = GetComponent<SpriteRenderer> ();
+		anim= GetComponent<Animator>();
+		bool doubleJump = true;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+		marcia-=decelerazione;
+		if(marcia>0)
+			transform.Translate(marcia, 0, 0);
+		/*else{
+			SceneManager.LoadScene("Scenes/Scena1", LoadSceneMode.Single);
+		}*/
+
+		
+		RaycastHit2D hitc = Physics2D.Raycast(transform.position, -Vector2.up, 0.8f, mask_secondo);
+       
+        
+        	if (hitc.collider == null) {
+					Physics2D.IgnoreLayerCollision(8, 11, true);
+					/*if(decelerazione==0)
+						marcia = movimento;*/
+	        }
+	        if (!IsGrounded && hitc.collider != null) {
+
+			Debug.Log("Doppio parry");
+					Physics2D.IgnoreLayerCollision(8, 11, false);
+					marcia = 2 * movimento;
+	        }
+
+
+		if(Input.GetButtonDown("Fire1") && (IsGrounded || doubleJump)){
+			anim.SetBool("jump", true);
+			if(!IsGrounded){
+				Debug.Log("Doppio salto");
+				doubleJump = false;
+				Physics2D.IgnoreLayerCollision(8, 11, false);
+			}
+			IsGrounded = false;
+			rb.velocity = new Vector2(rb.velocity.x, 0);
+			rb.AddForce(new Vector2(0, 200 * mod));
+		}
+		if(IsGrounded){
+			anim.SetBool("jump", false);
+		}
+
+		if(t>0){
+			t-=Time.deltaTime;
+		}
+		if (t < 0) {
+			//anim.SetBool("inciamp", false);
+			Physics2D.IgnoreLayerCollision (8, 10, false);
+			marcia *= 2;
+			t = 0;
+		}
+	}
+
+	void OnCollisionEnter2D (Collision2D coll) {				//upon touching ground reset vars
+		if (coll.gameObject.layer == 9) {
+			IsGrounded = true;						
+			doubleJump = true;
+			
+			Physics2D.IgnoreLayerCollision(8, 11, true);
+		}
+		if (coll.gameObject.layer == 10) {
+			Debug.Log ("Ouch");
+			//anim.SetBool("inciamp", true);
+			Physics2D.IgnoreLayerCollision (8, 10, true);
+			marcia /= 2;
+			t = 1.5f;
+		}
+		if (coll.gameObject.layer == 11) {
+			anim.SetBool("jump", false);
+			Debug.Log("roba");
+			decelerazione = 0.01f;
+		}
+		if (coll.gameObject.layer == 12) {
+			Debug.Log("roba");
+			decelerazione = 0.01f;
+		}
+
+	}
+
+	void OnCollisionStay2D (Collision2D coll) {
+		if (coll.gameObject.layer == 11) {
+			rb.gravityScale = 0.0f;
+		} 
+	}
+
+	void OnCollisionExit2D (Collision2D coll) {
+		rb.gravityScale = 1.0f;
+	}
+
+	public void example(){
+		Debug.Log ("Esperimento riuscito");
+	}
+}
