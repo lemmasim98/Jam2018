88bc97cdcae7b0a22ced3f8a0a947291c8d1c6e1
Simone <lemmasim98@gmail.com>
2018-01-27 09:45:21 +0100

    rere

--- JAM/Assets/Scripts/Jump2.cs
+++ JAM/Assets/Scripts/Jump2.cs
@@ -6,6 +6,7 @@ public class Jump2 : MonoBehaviour {
 	Rigidbody2D rb;
 	bool doubleJump = true;
 	public LayerMask mask;
+	public LayerMask mask2;
 	Animator anim;
 	// Use this for initialization
 	void Start () {
@@ -21,6 +22,13 @@ public class Jump2 : MonoBehaviour {
         if (hit.collider != null) {
         	IsGrounded = true;
         	doubleJump = true;
+        }
+        else{
+			RaycastHit2D hitc = Physics2D.Raycast(transform.position, -Vector2.up, 0.8f, mask2);
+	        if (hitc.collider != null) {
+					Physics2D.IgnoreLayerCollision(8, 11, false);
+	        }
+        	
         }
 
 
