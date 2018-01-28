9def675e57296b53fa937a5f389961f83e73dbbd
Franz <francescopessina96@mail.com>
2018-01-28 10:20:47 +0100

    Audio Hitbox

---
+++ JAM/Assets/Scripts/AudioCollision.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioCollision : MonoBehaviour {
+
+	public LayerMask mask_audio;
+	string AudioID;
+
+	// Use this for initialization
+	void Start () {
+		Physics2D.IgnoreLayerCollision (8, 14, true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RaycastHit2D hitAudio = Physics2D.Raycast(transform.position, Vector2.right, 0.8f, mask_audio);
+		if (hitAudio.collider != null) {
+			Debug.Log ("audio");
+			AudioID = hitAudio.collider.tag;
+			Debug.Log(AudioID);
+		}
+	}
+}
